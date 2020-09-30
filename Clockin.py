f9ee28b18861a094d17dafba1f35843f2752949e
谢杰华 <wb.xiejiehua@mesg.corp.netease.com>
2020-09-30 10:50:00 +0800

    Finished

--- Clockin.py
+++ Clockin.py
@@ -42,6 +42,8 @@ class ClockIn:
 
         # 检验员工是否还在职，如果一次打卡时间都没有，就判定为不在职
         def onjob(sheet, which_employee):
+            workday = 0
+            notworkday = 0
             if which_employee is 1:
                 start_index = (12,1)
                 end_index = (42, 12)
@@ -58,9 +60,14 @@ class ClockIn:
                     # 如果任意一个时间不是空的，那就是在职的
                     if morning_sb is not "" or morning_xb is not "" or noon_sb is not "" or noon_xb is not "" or \
                             jiaban_sb is not "" or jiaban_xb is not "":
-                        print(f"ddddd {morning_sb} {morning_xb} {noon_sb} {noon_xb} {jiaban_sb} {jiaban_xb}")
-                        return True
+                        workday+=1
+                        continue
+                    else:
+                        notworkday+=1
 
+                    if notworkday==31:
+                        return False, workday
+                return True, workday
             elif which_employee is 2:
                 start_index = (12,16)
                 end_index = (42, 27)
@@ -77,33 +84,42 @@ class ClockIn:
                     # 如果任意一个时间不是空的，那就是在职的
                     if morning_sb is not "" or morning_xb is not "" or noon_sb is not "" or noon_xb is not "" or \
                             jiaban_sb is not "" or jiaban_xb is not "":
-                        print(f"ddddd {morning_sb} {morning_xb} {noon_sb} {noon_xb} {jiaban_sb} {jiaban_xb}")
-                        return True
+                        workday+=1
+                        continue
+                    else:
+                        notworkday+=1
+
+                    if notworkday==31:
+                        return False, workday
+                return True, workday
             elif which_employee is 3:
-                start_index = (12,20)
-                end_index = (42, 31)
+                start_index = (12,31)
+                end_index = (42, 42)
                 # 遍历所有的时间点
                 for row in range(start_index[0], end_index[0]+1):
-                    morning_sb = sheet.cell_value(row, 20)
-                    morning_xb = sheet.cell_value(row, 22)
-                    noon_sb = sheet.cell_value(row, 25)
-                    noon_xb = sheet.cell_value(row, 27)
-                    jiaban_sb = sheet.cell_value(row, 29)
-                    jiaban_xb = sheet.cell_value(row, 31)
+                    morning_sb = sheet.cell_value(row, 31)
+                    morning_xb = sheet.cell_value(row, 33)
+                    noon_sb = sheet.cell_value(row, 36)
+                    noon_xb = sheet.cell_value(row, 38)
+                    jiaban_sb = sheet.cell_value(row, 40)
+                    jiaban_xb = sheet.cell_value(row, 42)
 
 
                     # 如果任意一个时间不是空的，那就是在职的
                     if morning_sb is not "" or morning_xb is not "" or noon_sb is not "" or noon_xb is not "" or \
                             jiaban_sb is not "" or jiaban_xb is not "":
-                        print(f"ddddd {morning_sb} {morning_xb} {noon_sb} {noon_xb} {jiaban_sb} {jiaban_xb}")
-                        return True
+                        workday+=1
+                        continue
+                    else:
+                        notworkday+=1
+
+                    if notworkday==31:
+                        return False, workday
+                return True, workday
             else:
-                return None
+                return None, None
 
 
-
-            print(f"{sheet} {which_employee} is not on job")
-            return False
 
         def EmployeeData(sheet, which_employee):
             '''
@@ -121,53 +137,77 @@ class ClockIn:
 
             employee_single_data = {}
 
-            if which_employee is 1:
+            if which_employee == 1:
                 start_index = (12,1)
                 end_index = (42, 12)
                 employeeName = sheet.cell_value(3, 9)
                 workNum = sheet.cell_value(4, 9)
-            elif which_employee is 2:
+                timeCol = [1, 3, 6, 8, 10, 12]
+            elif which_employee == 2:
                 start_index = (12,16)
                 end_index = (42, 27)
-                employeeName = sheet.cell_value(3, 9)
+                employeeName = sheet.cell_value(3, 24)
                 workNum = sheet.cell_value(4, 24)
-            elif which_employee is 3:
+                timeCol = [16, 18, 21, 23, 25, 27]
+            elif which_employee == 3:
                 start_index = (12,20)
                 end_index = (42, 31)
-                employeeName = sheet.cell_value(3, 9)
+                employeeName = sheet.cell_value(3, 39)
                 workNum = sheet.cell_value(4, 39)
+                timeCol = [31, 33, 36, 38, 40, 42]
             else:
                 return None
 
             day = 0
+            employee_single_data["name"] = employeeName
+            employee_single_data["workNum"] = int(workNum)
             for row in range(start_index[0], end_index[0]+1):
 
                 # 前面已经获得了工号以及名字
                 # 读取所有的打卡时间
                 clock_time = []
-                morning_sb = sheet.cell_value(row, 1)
-                morning_xb = sheet.cell_value(row, 3)
-                noon_sb = sheet.cell_value(row, 6)
-                noon_xb = sheet.cell_value(row, 8)
-                jiaban_sb = sheet.cell_value(row, 10)
-                jiaban_xb = sheet.cell_value(row, 12)
+                morning_sb = sheet.cell_value(row, timeCol[0])
+                morning_xb = sheet.cell_value(row, timeCol[1])
+                noon_sb = sheet.cell_value(row, timeCol[2])
+                noon_xb = sheet.cell_value(row, timeCol[3])
+                jiaban_sb = sheet.cell_value(row, timeCol[4])
+                jiaban_xb = sheet.cell_value(row, timeCol[5])
 
-                if morning_sb is not '': clock_time.append(morning_sb)
-                if morning_xb is not '': clock_time.append(morning_xb)
-                if noon_sb is not '': clock_time.append(noon_sb)
-                if noon_xb is not '': clock_time.append(noon_xb)
-                if jiaban_sb is not '': clock_time.append(jiaban_sb)
-                if jiaban_xb is not '': clock_time.append(jiaban_xb)
+                # if morning_sb is not '': print(xlrd.xldate_as_datetime(sheet.cell(row, timeCol[0]).value, 0))
+                # if morning_xb is not '': print(xlrd.xldate_as_datetime(sheet.cell(row, timeCol[1]).value, 0))
+                # if noon_sb is not '': print(xlrd.xldate_as_datetime(sheet.cell(row, timeCol[2]).value, 0))
+                # if noon_xb is not '': print(xlrd.xldate_as_datetime(sheet.cell(row, timeCol[3]).value, 0))
+                # if jiaban_sb is not '': print(xlrd.xldate_as_datetime(sheet.cell(row, timeCol[4]).value, 0))
+                # if jiaban_xb is not '': print(xlrd.xldate_as_datetime(sheet.cell(row, timeCol[5]).value, 0))
+
+                if morning_sb is not '':
+                    morning_sb = str(xlrd.xldate_as_datetime(sheet.cell(row, timeCol[0]).value, 0))[11:16]
+                    clock_time.append(morning_sb)
+                if morning_xb is not '':
+                    morning_xb = str(xlrd.xldate_as_datetime(sheet.cell(row, timeCol[1]).value, 0))[11:16]
+                    clock_time.append(morning_xb)
+                if noon_sb is not '':
+                    noon_sb = str(xlrd.xldate_as_datetime(sheet.cell(row, timeCol[2]).value, 0))[11:16]
+                    clock_time.append(noon_sb)
+                if noon_xb is not '':
+                    noon_xb = str(xlrd.xldate_as_datetime(sheet.cell(row, timeCol[3]).value, 0))[11:16]
+                    clock_time.append(noon_xb)
+                if jiaban_sb is not '':
+                    jiaban_sb = str(xlrd.xldate_as_datetime(sheet.cell(row, timeCol[4]).value, 0))[11:16]
+                    clock_time.append(jiaban_sb)
+                if jiaban_xb is not '':
+
+                    if type(jiaban_xb) is str:
+                        jiaban_xb = sheet.cell_value(row, timeCol[5])
+                    else:
+                        jiaban_xb = str(xlrd.xldate_as_datetime(sheet.cell(row, timeCol[5]).value, 0))[11:16]
+                    clock_time.append(jiaban_xb)
 
                 # 收集数据
-                employee_single_data["name"] = employeeName
-                employee_single_data["workNum"] = workNum
-                employee_single_data[f"day_{str(day+1)}"] = clock_time
+                day += 1
+                employee_single_data[f"day_{day}"] = clock_time
 
-                return employee_single_data
-
-
-            pass
+            return employee_single_data
 
 
         # 从左向右分别读取三位员工的基本信息
@@ -175,21 +215,25 @@ class ClockIn:
         for sheetIndex in range(2, 21):
             sheet = self.kqbb.sheet_by_index(sheetIndex)
             index+=1
-            print(f"这是第{index}个表")
             # 第一位员工
-            print(f"这是该表第一位员工")
-            if onjob(sheet, 1):
-                self.employee_data[self.employee_count+1] = EmployeeData(sheet, 1)
-
+            isonjob_1, workday = onjob(sheet, 1)
+            if isonjob_1:
+                self.employee_data[f"{index}"] = EmployeeData(sheet, 1)
+                self.employee_data[f"{index}"]["workday"] = workday
             # 第二位员工
-            print(f"这是该表第二位员工")
-            if onjob(sheet, 2):
-                self.employee_data[self.employee_count+1] = EmployeeData(sheet, 2)
-
+            isonjob_2, workday = onjob(sheet, 2)
+            if isonjob_2:
+                self.employee_data[f"{index}"] = EmployeeData(sheet, 2)
+                self.employee_data[f"{index}"]["workday"] = workday
             # 第三位员工
-            print(f"这是该表第三位员工")
-            if onjob(sheet, 3):
-                self.employee_data[self.employee_count+1] = EmployeeData(sheet, 3)
+            isonjob_3, workday = onjob(sheet, 3)
+            print(f"{isonjob_3}")
+            print(workday)
+            if isonjob_3:
+                self.employee_data[f"{index}"] = EmployeeData(sheet, 3)
+                self.employee_data[f"{index}"]["workday"] = workday
+            self.employee_data['kqrq'] = sheet.cell_value(1, 33)
+            self.employee_data['zbsj'] = sheet.cell_value(2, 33)
         return self.employee_data
 
     def run(self):
@@ -202,5 +246,7 @@ class ClockIn:
 
 
 # ClockIn().run()
-a = ClockIn()
-a.run()
+if __name__ == '__main__':
+
+    a = ClockIn()
+    a.run()
---
+++ createBlankTable.py
@@ -0,0 +1,389 @@
+# coding: utf-8
+# @Time    : 2020/9/20 10:51 下午
+# @Author  : 蟹蟹 ！！
+# @FileName: test.py
+# @Software: PyCharm
+
+import xlwt
+workbook = xlwt.Workbook()
+worksheet = workbook.add_sheet('员工刷卡记录')
+
+class color:
+    LIGHT_BLUE = 0xEF7D31
+    LIGHT_PINK = 45
+
+# 设置列宽
+a = worksheet.col(0)
+b = worksheet.col(1)
+c = worksheet.col(2)
+d = worksheet.col(3)
+e = worksheet.col(4)
+f = worksheet.col(5)
+g = worksheet.col(6)
+h = worksheet.col(7)
+i = worksheet.col(8)
+k = worksheet.col(9)
+l = worksheet.col(10)
+m = worksheet.col(11)
+n = worksheet.col(12)
+o = worksheet.col(13)
+p = worksheet.col(14)
+q = worksheet.col(15)
+r = worksheet.col(16)
+s = worksheet.col(17)
+t = worksheet.col(18)
+u = worksheet.col(19)
+v = worksheet.col(20)
+w = worksheet.col(21)
+x = worksheet.col(22)
+y = worksheet.col(23)
+z = worksheet.col(24)
+aa = worksheet.col(25)
+ab = worksheet.col(26)
+ac = worksheet.col(27)
+ad = worksheet.col(28)
+ae = worksheet.col(29)
+af = worksheet.col(30)
+ag = worksheet.col(31)
+ah = worksheet.col(32)
+ai = worksheet.col(33)
+ak = worksheet.col(34)
+al = worksheet.col(35)
+
+a.width = 256*6
+b.width = 256*6
+c.width = 256*6
+d.width = 256*6
+e.width = 256*6
+f.width = 256*6
+g.width = 256*6
+h.width = 256*6
+i.width = 256*6
+k.width = 256*6
+l.width = 256*6
+m.width = 256*6
+n.width = 256*6
+o.width = 256*6
+p.width = 256*6
+q.width = 256*6
+r.width = 256*6
+s.width = 256*6
+t.width = 256*6
+u.width = 256*6
+v.width = 256*6
+w.width = 256*6
+x.width = 256*6
+y.width = 256*6
+z.width = 256*6
+aa.width = 256*6
+ab.width = 256*6
+ac.width = 256*6
+ad.width = 256*6
+ae.width = 256*6
+af.width = 256*6
+ag.width = 256*6
+ah.width = 256*6
+ai.width = 256*6
+ak.width = 256*6
+al.width = 256*6
+
+
+
+# 标题字体
+font_title = xlwt.Font()
+font_title.name = 'name Times New Roman'
+font_title.height = 20*24
+font_title.bold = True
+
+# 设置单元格对齐方式
+alignment_title = xlwt.Alignment()
+    # 0x01(左端对齐)、0x02(水平方向上居中对齐)、0x03(右端对齐)
+alignment_title.horz = 0x02
+    # 0x00(上端对齐)、 0x01(垂直方向上居中对齐)、0x02(底端对齐)
+alignment_title.vert = 0x01
+
+# 初始化样式
+style_title = xlwt.XFStyle()
+style_title.font = font_title
+style_title.alignment = alignment_title
+
+# 获取数据
+from Clockin import ClockIn
+data = ClockIn().employee()
+worksheet.write_merge(0, 4, 8, 23, '员 工 刷 卡 记 录 表', style_title)
+worksheet.write_merge(2, 2 , 25,31, data['kqrq'])
+worksheet.write_merge(3, 3, 25,31, data['zbsj'])
+
+row_first_index = 0
+input_data = ''
+third_row_index = 2
+try:
+    # 遍历每个员工
+    for j in data.keys():  # 第一个员工
+        dayEachMember = 0
+        if j == 'kqrq' or j == 'zbsj':
+            continue
+        print(f"第{j}个员工打卡时间录入。。。")
+
+        row_first_index += 5
+        third_row_index += 5
+
+        # region 行高设置
+        # 设置行高
+        row_first = worksheet.row(row_first_index)
+        row_first_height_style = xlwt.easyxf('font:height 500;')
+        row_first.set_style(row_first_height_style)
+        #endregion
+
+        # region 第一行style
+        # 第一格子，工号style
+        font_workNum = xlwt.Font()
+        font_workNum.height = 20*11
+        font_workNum.bold = True
+        alignment_workNum = xlwt.Alignment()
+        alignment_workNum.horz = 0x02
+        alignment_workNum.vert = 0x01
+        borders_workNum = xlwt.Borders()
+        borders_workNum.left = 5
+        borders_workNum.top = 5
+        borders_workNum.bottom = 5
+        borders_workNum.left_colour = color.LIGHT_BLUE
+        borders_workNum.top_colour = color.LIGHT_BLUE
+        borders_workNum.bottom_colour = color.LIGHT_BLUE
+        style_workNum = xlwt.XFStyle()
+        style_workNum.font = font_workNum
+        style_workNum.alignment = alignment_workNum
+        style_workNum.borders = borders_workNum
+
+        # 第一个姓名
+        font_name = xlwt.Font()
+        font_name.height = 20*11
+        font_name.bold = True
+        alignment_name = xlwt.Alignment()
+        alignment_name.horz = 0x02
+        alignment_name.vert = 0x01
+        borders_name = xlwt.Borders()
+        borders_name.top = 5
+        borders_name.bottom = 5
+        borders_name.top_colour = color.LIGHT_BLUE
+        borders_name.bottom_colour = color.LIGHT_BLUE
+        style_name = xlwt.XFStyle()
+        style_name.font = font_name
+        style_name.alignment = alignment_name
+        style_name.borders = borders_name
+
+        # 第一个部门
+        font_comp = xlwt.Font()
+        font_comp.height = 20*11
+        font_comp.bold = True
+        alignment_comp = xlwt.Alignment()
+        alignment_comp.horz = 0x02
+        alignment_comp.vert = 0x01
+        borders_comp = xlwt.Borders()
+        borders_comp.top = 5
+        borders_comp.bottom = 5
+        borders_comp.top_colour = color.LIGHT_BLUE
+        borders_comp.bottom_colour = color.LIGHT_BLUE
+        style_comp = xlwt.XFStyle()
+        style_comp.font = font_comp
+        style_comp.alignment = alignment_comp
+        style_comp.borders = borders_comp
+
+        # 第一个公司名字
+        font_compName = xlwt.Font()
+        font_compName.height = 20*11
+        font_compName.bold = True
+        alignment_compName = xlwt.Alignment()
+        alignment_compName.horz = 0x02
+        alignment_compName.vert = 0x01
+        borders_compName = xlwt.Borders()
+        borders_compName.top = 5
+        borders_compName.bottom = 5
+        borders_compName.top_colour = color.LIGHT_BLUE
+        borders_compName.bottom_colour = color.LIGHT_BLUE
+        style_compName = xlwt.XFStyle()
+        style_compName.font = font_compName
+        style_compName.alignment = alignment_compName
+        style_compName.borders = borders_compName
+
+        #endregion
+
+        # region 第一行数据填入
+        worksheet.write_merge(row_first_index, row_first_index, 1,2, '工号：', style_workNum)
+        worksheet.write(row_first_index, 3,  data[j]["workNum"], style_workNum)
+        for i in range(4, 9):
+            borders_first_row = xlwt.Borders()
+            borders_first_row.top = 5
+            borders_first_row.bottom = 5
+            borders_first_row.top_colour = color.LIGHT_BLUE
+            borders_first_row.bottom_colour = color.LIGHT_BLUE
+            style_first_row = xlwt.XFStyle()
+            style_first_row.borders = borders_first_row
+            worksheet.write(row_first_index, i, '', style_first_row)
+        worksheet.write_merge(row_first_index, row_first_index, 9,10, '姓名：', style_name)
+        worksheet.write_merge(row_first_index, row_first_index, 11,12, data[j]["name"], style_name)
+
+
+        for i in range(13, 16):
+            borders_first_row = xlwt.Borders()
+            borders_first_row.top = 5
+            borders_first_row.bottom = 5
+            borders_first_row.top_colour = color.LIGHT_BLUE
+            borders_first_row.bottom_colour = color.LIGHT_BLUE
+            style_first_row = xlwt.XFStyle()
+            style_first_row.borders = borders_first_row
+            worksheet.write(row_first_index, i, '', style_first_row)
+        worksheet.write_merge(row_first_index, row_first_index, 16,17, '部门：', style_comp)
+        worksheet.write_merge(row_first_index, row_first_index, 18,19, '建利灯配', style_compName)
+        for i in range(20, 33):
+            borders_first_row = xlwt.Borders()
+            borders_first_row.top = 5
+            borders_first_row.bottom = 5
+            borders_first_row.top_colour = color.LIGHT_BLUE
+            borders_first_row.bottom_colour = color.LIGHT_BLUE
+            style_first_row = xlwt.XFStyle()
+            style_first_row.borders = borders_first_row
+            worksheet.write(row_first_index, i, '', style_first_row)
+        # 收尾
+        borders_first_row = xlwt.Borders()
+        borders_first_row.top = 5
+        borders_first_row.right = 5
+        borders_first_row.bottom = 5
+        borders_first_row.top_colour = color.LIGHT_BLUE
+        borders_first_row.right_colour = color.LIGHT_BLUE
+        borders_first_row.bottom_colour = color.LIGHT_BLUE
+        style_first_row = xlwt.XFStyle()
+        style_first_row.borders = borders_first_row
+        worksheet.write(row_first_index, 33, '', style_first_row)
+        #endregion
+
+        # region 第二行数据
+        second_row = worksheet.row(row_first_index+1)
+        second_height = xlwt.easyxf('font:height 500;')
+        second_row.set_style(second_height)
+        for i in range(1,34):
+            # 第一个的话
+            if i == 1:
+                borders_second_row = xlwt.Borders()
+                borders_second_row.left = 5
+                borders_second_row.bottom = 2
+                borders_second_row.right = 2
+                borders_second_row.left_colour = color.LIGHT_BLUE
+                borders_second_row.bottom_colour = color.LIGHT_PINK
+                borders_second_row.right_colour = color.LIGHT_PINK
+                style_second_row = xlwt.XFStyle()
+                style_second_row.borders = borders_second_row
+
+            # 最后一个
+            elif i == 33:
+                borders_second_row = xlwt.Borders()
+                borders_second_row.bottom = 2
+                borders_second_row.right = 5
+                borders_second_row.bottom_colour = color.LIGHT_PINK
+                borders_second_row.right_colour = color.LIGHT_BLUE
+                style_second_row = xlwt.XFStyle()
+                style_second_row.borders = borders_second_row
+            else:
+                # 第二个以及以后
+                borders_second_row = xlwt.Borders()
+                borders_second_row.bottom = 2
+                borders_second_row.right = 2
+                borders_second_row.bottom_colour = color.LIGHT_PINK
+                borders_second_row.right_colour = color.LIGHT_PINK
+                style_second_row = xlwt.XFStyle()
+                style_second_row.borders = borders_second_row
+
+            if i == 32 :
+                style_second_row.alignment.wrap = 1
+                worksheet.write(row_first_index+1, i, '上班时间', style_second_row)
+            elif i == 33:
+                style_second_row.alignment.wrap = 1
+                worksheet.write(row_first_index+1, i, '加班小时', style_second_row)
+            else:
+                worksheet.write(row_first_index+1, i, f'{i}', style_second_row)
+
+        #endregion
+
+        # region 第三行数据
+
+        while True:
+            dayEachMember+=1
+            if dayEachMember <= 31:
+                ct_list = data[j][f'day_{dayEachMember}'] # 获取第1天数据
+                input_data = "\n".join(ct_list)
+            elif dayEachMember in [32, 33]:
+                pass
+            else:
+                break
+
+
+            # 填入打卡时间/上班天数/加班时间
+            third_row = worksheet.row(row_first_index+2)
+            third_height = xlwt.easyxf('font:height 1000;')
+            third_row.set_style(third_height)
+            #   筛选应该选用什么样的style
+            # 第一个的话
+            if dayEachMember == 1:
+                borders_third_row = xlwt.Borders()
+                borders_third_row.left = 5
+                borders_third_row.bottom = 2
+                borders_third_row.right = 2
+                borders_third_row.left_colour = color.LIGHT_BLUE
+                borders_third_row.bottom_colour = color.LIGHT_PINK
+                borders_third_row.right_colour = color.LIGHT_PINK
+                style_third_row = xlwt.XFStyle()
+                style_third_row.alignment.wrap = 1  #设置自动换行
+                style_third_row.borders = borders_third_row
+            elif dayEachMember == 33:
+                borders_third_row = xlwt.Borders()
+                borders_third_row.bottom = 2
+                borders_third_row.right = 5
+                borders_third_row.bottom_colour = color.LIGHT_PINK
+                borders_third_row.right_colour = color.LIGHT_BLUE
+                style_third_row = xlwt.XFStyle()
+                style_third_row.alignment.wrap = 1  #设置自动换行
+                style_third_row.borders = borders_third_row
+            elif dayEachMember == 32:
+                # 倒数第二个
+                borders_third_row = xlwt.Borders()
+                borders_third_row.bottom = 2
+                borders_third_row.right = 2
+                borders_third_row.bottom_colour = color.LIGHT_PINK
+                borders_third_row.right_colour = color.LIGHT_PINK
+                style_third_row = xlwt.XFStyle()
+                style_third_row.alignment.wrap = 1  #设置自动换行
+                style_third_row.borders = borders_third_row
+            else:
+                # 第二个以及以后
+                borders_third_row = xlwt.Borders()
+                borders_third_row.bottom = 2
+                borders_third_row.right = 2
+                borders_third_row.bottom_colour = color.LIGHT_PINK
+                borders_third_row.right_colour = color.LIGHT_PINK
+                style_third_row = xlwt.XFStyle()
+                style_third_row.alignment.wrap = 1  #设置自动换行
+                style_third_row.borders = borders_third_row
+
+
+            # 录入所有的打卡时间
+            if input_data is not '' and dayEachMember != 33 and dayEachMember != 32:
+                print(f"第{dayEachMember}天录入")
+                worksheet.write(third_row_index, dayEachMember, input_data, style_third_row)
+            elif dayEachMember == 32:
+                worksheet.write(third_row_index, dayEachMember, data[j]['workday'], style_third_row)
+            elif dayEachMember == 33 :
+                worksheet.write(third_row_index, dayEachMember, '', style_third_row)
+            else:
+                worksheet.write(third_row_index, dayEachMember, '', style_third_row)
+                print(f"第{dayEachMember}天没有打卡时间")
+                continue
+            from time import strftime, localtime
+
+            filename = strftime("%Y年%m月员工刷卡记录表", localtime())
+            workbook.save(filename)
+
+except:
+    workbook.save('Merge_cell.xls')
+
+import os
+os.system(f"open {filename}")
